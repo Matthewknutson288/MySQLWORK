ec9ae0e3e1859c3615a1ebea18659bb5dd9fb91a
Matthewknutson288 <matthewknutson288@gmail.com>
2024-05-11 10:55:40 -0700

    final commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,14 @@
+package projects.dao;
+
+public class DbConnection {
+
+	public DbConnection() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+private static String HOST = "localhost";
+private static String PASSWORD = "projects";
+private static int PORT = 3306;
+private static String SCHEMA = "projects";
+private static String USER = "projects";
---
+++ src/main/java/projects/exception/DbException.java
@@ -0,0 +1,13 @@
+package projects.exception;
+
+public class DbException {
+
+	public DbException() {
+		// TODO Auto-generated constructor stub
+	}
+public DbException(String message) {}
+
+public DbException(Throwable cause) {}
+
+public DbException(String message, Throwable cause) {}
+}
